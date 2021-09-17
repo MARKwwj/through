c6f51f3ce51f5ef45210ba09ff6bd2e471653330
jun747 <319351912@qq.com>
2021-09-17 22:14:13 +0800

    1

--- server/server.go
+++ server/server.go
@@ -151,7 +151,7 @@ func establishTunnel(tunnel *net.TCPConn) {
 
 	for key, connMatch := range connectionPool {
 		if connMatch.accept != nil {
-			go network.Join2Conn(connMatch.accept, tunnel)
+			go network.Join2Conn(tunnel, connMatch.accept)
 			delete(connectionPool, key)
 			return
 		}
